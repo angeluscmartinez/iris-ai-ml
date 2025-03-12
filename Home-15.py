e6690d4def616eee1dcb68233b15a9172c2bb5ed
angeluscmartinez <111898653+angeluscmartinez@users.noreply.github.com>
2025-03-11 20:26:38 -0700

    Add files via upload

--- Home-15.py
+++ Home-15.py
@@ -6,7 +6,8 @@ from streamlit_lottie import st_lottie
 from fuzzywuzzy import process
 import io
 
-openai.api_key = "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"
+#openai.api_key = "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"
+openai.api_key = st.secrets["API_key"]
 
 def load_lottiefile(filepath: str):
     with open(filepath, "r") as f:
